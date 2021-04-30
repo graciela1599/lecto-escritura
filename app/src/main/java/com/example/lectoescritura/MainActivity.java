8dcf5be358c994551e1ea6bfa64ff928e476f7fd
Graciela <17170367@itculiacan.edu.mx>
2021-04-30 05:25:10 -0600

    Pantalla inicio lectoescritura

--- app/src/main/java/com/example/lectoescritura/MainActivity.java
+++ app/src/main/java/com/example/lectoescritura/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.lectoescritura;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    Dialog myDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void btn_next(View view) {
+    }
+
+    public void btn_about(View view) {
+        TextView txtClose;
+        myDialog.setContentView(R.layout.about_info);
+        txtClose = (TextView) myDialog.findViewById(R.id.close_about);
+
+        txtClose.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                myDialog.dismiss();
+            }
+        });
+        myDialog.show();
+    }
+
 }
