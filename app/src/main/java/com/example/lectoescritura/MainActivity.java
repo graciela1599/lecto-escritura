0793576858823bb40580c70a4451f4d19bbee0c4
Graciela <17170367@itculiacan.edu.mx>
2021-04-30 17:49:07 -0600

    Corrección Popup Window

--- app/src/main/java/com/example/lectoescritura/MainActivity.java
+++ app/src/main/java/com/example/lectoescritura/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.lectoescritura;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Dialog;
+import android.app.DirectAction;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     Dialog myDialog;
+    ImageView btn_about;
+    TextView txtClose;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,16 +29,24 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void btn_about(View view) {
-        TextView txtClose;
-        myDialog.setContentView(R.layout.about_info);
-        txtClose = (TextView) myDialog.findViewById(R.id.close_about);
 
-        txtClose.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                myDialog.dismiss();
+
+        LayoutInflater inflater = (LayoutInflater)
+                getSystemService(LAYOUT_INFLATER_SERVICE);
+        View popupView = inflater.inflate(R.layout.about_info, null);
+        int width = LinearLayout.LayoutParams.WRAP_CONTENT, height = width;
+        boolean focused = true;
+        final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focused);
+        popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
+
+        // dismiss the popup window when touched
+        popupView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                popupWindow.dismiss();
+                return true;
             }
         });
-        myDialog.show();
     }
 
 }
