f1fe9cd43efa6a2e8e6a52a701c80068bcbf7118
Graciela <17170367@itculiacan.edu.mx>
2021-05-13 22:39:32 -0600

    Cambiando pantalla de error

--- app/src/main/java/com/example/lectoescritura/Nivel1_Error.java
+++ app/src/main/java/com/example/lectoescritura/Nivel1_Error.java
@@ -2,19 +2,23 @@ package com.example.lectoescritura;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 
 public class Nivel1_Error extends AppCompatActivity {
-
+    Window window;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nivel1__error);
+        if(Build.VERSION.SDK_INT >=21){
+            window=this.getWindow();
+            window.setStatusBarColor(this.getResources().getColor(R.color.sand));
+        }
     }
     public void Error(View v) {
         onBackPressed();
-
-
     }
 }
